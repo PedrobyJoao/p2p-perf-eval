5440f442c95c8e67c7ad7c909c3ba7bb330cbecd
PedroByJoao <joaoexplorer235@gmail.com>
2025-06-21 17:22:27 -0300

    refact: go back to go

--- p2p/flake.nix
+++ p2p/flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "Nim project flake";
+  description = "poc p2p analysis";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,26 +10,58 @@
     { self
     , nixpkgs
     , flake-utils
-    , ...
+    ,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
       in
       {
+        packages = rec {
+          p2p-analysis = pkgs.buildGoModule {
+            pname = "p2p-analysis";
+            # inherit version;
+            src = ./.;
+            vendorHash = null; # Let nix compute the vendor hash
+
+            buildInputs = with pkgs; [
+              pkg-config
+              sqlite
+            ];
+
+            # Add any necessary build flags here
+            buildFlags = [
+              "-tags=libsqlite3"
+            ];
+
+          };
+          default = p2p-analysis;
+        };
 
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nim
-            nimble
-            nimlangserver
+            go
+            gopls
+            gotools
+            go-tools
+            sqlite
+            pkg-config
+            systemd.dev
+
+            # dev tools
+            zip
+            gnumake
           ];
 
-          # so when you run `nix develop` your ~/.nimble/bin
-          # is also in PATH, for per-user nimble installs:
           shellHook = ''
-            export PATH=$HOME/.nimble/bin:$PATH
+            export CGO_ENABLED=1
+            export PKG_CONFIG_PATH="${pkgs.systemd.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+            # solves: https://github.com/NVIDIA/nvidia-container-toolkit/issues/49
+            export GOFLAGS="-ldflags=-extldflags=-Wl,-z,lazy"
           '';
         };
       }
